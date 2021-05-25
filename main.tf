360d9718332d719560027ca04f915913878f3b05
aamir7tide <aamir.sahib@tide.co>
2021-05-25 22:36:25 +0530

    Added: Code to add aws IGW

--- main.tf
+++ main.tf
@@ -4,10 +4,23 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+# Defining variables
+variable "tf_vpc" {
+  default = "10.2.0.0/16"
+}
+
 # Create a VPC
 resource "aws_vpc" "terraform-vpc" {
-  cidr_block = "10.2.0.0/16"
+  cidr_block = var.tf_vpc
   tags = {
-    "name" = "Terrafrom_VPC"
+    "Name" = "Terrafrom_VPC"
   }
+}
+
+# Create a Internet Gateway
+resource "aws_internet_gateway" "terraform-igw" {
+    vpc_id = aws_vpc.terraform-vpc.id
+    tags = {
+      "Name" = "Terraform_IGW"
+    }
 }
